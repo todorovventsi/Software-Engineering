6be12d997a18344234308e4c61f0b739546e6fd2
V. Todorov - work <todorov.ventsi@gmail.com>
2021-02-24 10:21:07 +0200

    01. Zeros to back

--- Programming-Fundamentals-with-Python/lists-basics-m.exercises/01. Zeros to Back.py
+++ Programming-Fundamentals-with-Python/lists-basics-m.exercises/01. Zeros to Back.py
@@ -1,9 +1,7 @@
-number_list = input().split(", ")
-
-for index, value in enumerate(number_list):
-    if value == "0":
-        number_list.pop(index)
-        number_list.extend(value)
-new_numbers = [int(i) for i in number_list]
-
-print(new_numbers)
+numbers = list(map(int, input().split(", ")))
+zeros = numbers.count(0)
+numbers = list(filter(lambda x: not x == 0, numbers))
+# for _ in range(0, zeros):
+#     numbers.append(0)
+numbers.extend([0] * zeros)
+print(numbers)
