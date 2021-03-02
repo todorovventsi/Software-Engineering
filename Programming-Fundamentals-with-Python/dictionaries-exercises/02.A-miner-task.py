3086c5c9b42365a860239ae29f3251f2f22f53a6
V. Todorov - work <todorov.ventsi@gmail.com>
2021-03-02 16:19:25 +0200

    02.Miner task

---
+++ Programming-Fundamentals-with-Python/dictionaries-exercises/02.A-miner-task.py
@@ -0,0 +1,18 @@
+command = input()
+counter = 1
+resources = {}
+last_res = command
+
+while not command == "stop":
+    if not counter % 2 == 0: # when counter is odd
+        if command not in resources:
+            resources[command] = 0
+        last_res = command
+    else:
+        resources[last_res] += int(command)
+
+    counter += 1
+    command = input()
+
+for resource, value in resources.items():
+    print(f"{resource} -> {value}")
