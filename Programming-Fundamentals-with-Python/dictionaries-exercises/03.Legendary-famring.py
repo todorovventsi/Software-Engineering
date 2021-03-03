c27acdf670670a1885023cedb4d74113b765a196
todorovventsi <todorov.ventsi@gmail.com>
2021-03-03 14:43:19 +0200

    03.Legendary farming - edited

--- Programming-Fundamentals-with-Python/dictionaries-exercises/03.Legendary-famring.py
+++ Programming-Fundamentals-with-Python/dictionaries-exercises/03.Legendary-famring.py
@@ -1,30 +1,19 @@
 resources = {"shards": 0, "fragments": 0, "motes": 0}
-data = input().split()
 legendary_is_collected = False
-for idx in range(0, len(data), 2):
-    res = data[idx + 1].lower()
-    value = int(data[idx])
-    if res not in resources:
-        resources[res] = 0
-    resources[res] += value
-    if resources.get("shards") >= 250 or resources.get("fragments") >= 250 or resources.get("motes") >= 250:
-        legendary_is_collected = True
-        break
 
-if not legendary_is_collected:
-    while True:
-        data = input().split()
-        for idx in range(0, len(data), 2):
-            resource = data[idx + 1].lower()
-            quantity = int(data[idx])
-            if resource not in resources:
-                resources[resource] = 0
-            resources[resource] += quantity
-            if resources.get("shards") >= 250 or resources.get("fragments") >= 250 or resources.get("motes") >= 250:
-                legendary_is_collected = True
-                break
-        if legendary_is_collected:
+while True:
+    data = input().split()
+    for idx in range(0, len(data), 2):
+        resource = data[idx + 1].lower()
+        quantity = int(data[idx])
+        if resource not in resources:
+            resources[resource] = 0
+        resources[resource] += quantity
+        if resources.get("shards") >= 250 or resources.get("fragments") >= 250 or resources.get("motes") >= 250:
+            legendary_is_collected = True
             break
+    if legendary_is_collected:
+        break
 
 key_material = [key for key, value in resources.items() if value >= 250]
 resources[key_material[0]] -= 250
@@ -39,7 +28,6 @@ elif key_material[0] == "motes":
 key_materials_ls = ["shards", "fragments", "motes"]
 key_materials_dc = {item: value for item, value in resources.items() if item in key_materials_ls}
 junk_materials = {item: value for item, value in resources.items() if item not in key_materials_ls}
-
 
 key_mat_sorted = sorted(key_materials_dc, key = lambda k: (-key_materials_dc[k], k))
 junk_mat_sorted = sorted(junk_materials)
