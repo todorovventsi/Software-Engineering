1e9d445a4f2961238c70f4d23aee0b607a42c812
V. Todorov - work <todorov.ventsi@gmail.com>
2021-03-12 13:52:46 +0200

    06.Replace repeating string

--- Programming-Fundamentals-with-Python/text-processing-exercices/06.Replace-repeating-chars.py
+++ Programming-Fundamentals-with-Python/text-processing-exercices/06.Replace-repeating-chars.py
@@ -1,7 +1,13 @@
 string = input()
 
+index = 0
 
-for index in range(len(string) - 1):
-    pass
+while index < len(string) - 1:
+    if string[index] == string[index + 1]:
+        to_replace = f"{string[index]}{string[index + 1]}"
+        string = string.replace(to_replace, string[index])
+        index = 0
+    else:
+        index += 1
 
 print(string)
