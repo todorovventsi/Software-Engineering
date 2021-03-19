0b69d68385ca4f92660da984560b48bb0c73515d
V. Todorov - work <todorov.ventsi@gmail.com>
2021-03-19 13:46:43 +0200

    ...

--- Programming-Fundamentals-with-Python/regular-expressions/04.Match-numbers.py
+++ Programming-Fundamentals-with-Python/regular-expressions/04.Match-numbers.py
@@ -3,7 +3,6 @@ import re
 numbers_sequence = input()
 pattern = r'(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))'
 
-valid_numbers = re.finditer(pattern, numbers_sequence)
+valid_numbers = [m_object.group() for m_object in re.finditer(pattern, numbers_sequence)]
 
-for number in valid_numbers:
-    print(number.group(), end=" ")
+print(*valid_numbers)
