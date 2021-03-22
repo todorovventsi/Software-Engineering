bd68b52721010a50792e412eeb3138d5e316e8e9
V. Todorov - work <todorov.ventsi@gmail.com>
2021-03-22 12:15:05 +0200

    ...

--- Programming-Fundamentals-with-Python/regular-expressions-exersices/04.Extract-emails.py
+++ Programming-Fundamentals-with-Python/regular-expressions-exersices/04.Extract-emails.py
@@ -1,6 +1,6 @@
 import re
 
 emails = input()
-pattern = r'\b[a-zA-Z0-9]+([\.\-\_]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]+\-?[a-zA-Z0-9]+)(\.[a-zA-Z0-9]+\-?[a-zA-Z0-9]+)+'
+pattern = r'(^|(?<=\s))[a-zA-Z0-9]+[\._-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+'
 valid_emails = [matched_obj.group() for matched_obj in re.finditer(pattern, emails)]
 print(*valid_emails, sep="\n")
