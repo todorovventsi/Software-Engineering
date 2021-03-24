85450b7e179eb962b5035f37445f6c0c386e571b
V. Todorov - work <todorov.ventsi@gmail.com>
2021-03-24 11:48:07 +0200

    03.Final Exam Prep

---
+++ Programming-Fundamentals-with-Python/final-exam-preparation/03.Programming-fundamentals-final-exam/02.Mirror-words.py
@@ -0,0 +1,24 @@
+import re
+
+text = input()
+
+pattern = r"(?P<del>[@#])(?P<wordone>[a-zA-Z]{3,})(?P=del){2}(?P<wordtwo>[a-zA-Z]{3,})(?P=del)"
+valid_pairs = re.finditer(pattern, text)
+matches = [match.groupdict() for match in valid_pairs]
+if not matches:
+    print("No word pairs found!")
+    print("No mirror words!")
+else:
+    print(f"{len(matches)} word pairs found!")
+    mirrors = {}
+    for index in range(len(matches)):
+        if matches[index]['wordone'] == matches[index]['wordtwo'][::-1]:
+            mirrors[f"Pair {index + 1}"] = [matches[index]['wordone'], matches[index]['wordtwo']]
+    if mirrors:
+        print("The mirror words are:")
+        output = []
+        for pair, values in mirrors.items():
+            output.append(f"{values[0]} <=> {values[1]}")
+        print(*output, sep=", ")
+    else:
+        print("No mirror words!")
