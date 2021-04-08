149f4ea81eb776935268fa9e8e3ac511ee95d762
V. Todorov - work <todorov.ventsi@gmail.com>
2021-04-08 16:25:24 +0300

    Obj and cls - task added

---
+++ Programming-Fundamentals-with-Python/objects_and_classes-m.exercices/05.Class.py
@@ -0,0 +1,32 @@
+from statistics import mean
+
+
+class Class:
+    __students_count = 22
+
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        self.grades = []
+
+    def add_student(self, name, grade):
+        if len(self.students) < Class.__students_count:
+            self.students.append(name)
+            self.grades.append(grade)
+
+    def get_average_grade(self):
+        if self.grades:
+            return f"{mean(self.grades):.2f}"
+        return "0.00"
+
+    def __repr__(self):
+        students = ', '.join(self.students)
+        average = self.get_average_grade()
+        return f"The students in {self.name}: {students}. Average grade: {average}"
+
+
+a_class = Class("11B")
+# a_class.add_student("Peter", 4.80)
+# a_class.add_student("George", 6.00)
+# a_class.add_student("Amy", 3.50)
+print(a_class)
