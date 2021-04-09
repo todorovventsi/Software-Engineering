18f9d456d4e4726b0d25a62a5b3dbdcdc08c5ba6
V. Todorov - work <todorov.ventsi@gmail.com>
2021-04-09 11:20:59 +0300

    Obj and cls - task added

---
+++ Programming-Fundamentals-with-Python/objects_and_classes-m.exercices/08.Vehicle.py
@@ -0,0 +1,38 @@
+class Vehicle:
+
+    def __init__(self, type, model, price):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = None
+
+    def buy(self, money, owner):
+        if money < self.price:
+            return "Sorry, not enough money"
+        if self.owner:
+            return "Car already sold"
+        self.owner = owner
+        diff = abs(money - self.price)
+        return f"Successfully bought a {self.type}. Change: {diff:.2f}"
+
+    def sell(self):
+        if self.owner:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        return f"{self.model} {self.type} is on sale: {self.price}"
+
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+vehicle.buy(15000, "Peter")
+vehicle.buy(35000, "George")
+print(vehicle)
+vehicle.sell()
+print(vehicle)
