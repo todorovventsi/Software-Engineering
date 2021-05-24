374c1d464a819bd3895e569693a8bc9a2e1e9665
todorovventsi <todorov.ventsi@gmail.com>
2021-05-24 13:32:33 +0300

    problem added

---
+++ Python-Advanced-2021/01.Lists-as-stacks-and-queues-E/02.Max-and-min-element.py
@@ -0,0 +1,17 @@
+number_of_queries = int(input())
+stack_state = []
+
+for _ in range(number_of_queries):
+    command = input().split()
+    if command[0] == "1":
+        stack_state.append(int(command[1]))
+    elif command[0] == "2":
+        if stack_state:
+            stack_state.pop()
+    elif command[0] == "3":
+        print(max(stack_state))
+    elif command[0] == "4":
+        print(min(stack_state))
+if stack_state:
+    stack_print = [stack_state.pop() for _ in range(len(stack_state))]
+    print(*stack_print, sep=", ")
