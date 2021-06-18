16d9be68ae93db34d625d1a69874241dae30e07a
V. Todorov - work <todorov.ventsi@gmail.com>
2021-06-18 13:02:38 +0300

    ...

---
+++ Python-Advanced-2021/05.Functions-advanced-E/12.Recursion_palindrome.py
@@ -0,0 +1,11 @@
+def palindrome(text, index=0):
+    right_index = len(text) - 1 - index
+    index_diff = index - right_index
+    if not text[index] == text[right_index]:
+        return f"{text} is not a palindrome"
+    if abs(index_diff) <= 1:
+        return f"{text} is a palindrome"
+    return palindrome(text, index + 1)
+
+print(palindrome("peter", 0))
+print(palindrome("abcba", 0))
--- Python-Advanced-2021/demo.py
+++ Python-Advanced-2021/demo.py
@@ -1,6 +1,6 @@
 def recursive_power(number, power):
-    if power == 1:
-        return number
+    if power == 0:
+        return 1
     return number * recursive_power(number, power - 1)
 
-print(recursive_power(2, 10))
+print(recursive_power(2, 4))
