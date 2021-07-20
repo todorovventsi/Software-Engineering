d4f13613eb8e2084d88c98d981b0b213f293a503
V. Todorov - work <todorov.ventsi@gmail.com>
2021-07-20 13:58:53 +0300

    ...

---
+++ Python-OOP-July2021/12.Polymorphism_and_abstraction-E/groups.py
@@ -0,0 +1,62 @@
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __repr__(self):
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name, people):
+        self.i = 0
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        name = f"{self.name} {other.name}"
+        people = self.people + other.people
+        return Group(name, people)
+
+    def __repr__(self):
+        people = [f"{person!r}" for person in self.people]
+        return f"Group {self.name} with members {', '.join(people)}"
+
+    def __getitem__(self, i):
+        person = self.people[i]
+        return f"Person {i}: {person!r}"
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < len(self.people):
+            item = self.people[self.i]
+            result = f"Person {self.i}: {item!r}"
+            self.i += 1
+            return result
+        raise StopIteration
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)
