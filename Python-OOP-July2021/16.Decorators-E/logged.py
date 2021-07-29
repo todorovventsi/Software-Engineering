93fdbc5efaa5c955e7f47398f800e1e6fee02d59
V. Todorov - work <todorov.ventsi@gmail.com>
2021-07-29 14:07:37 +0300

    Decorators Exercise

---
+++ Python-OOP-July2021/16.Decorators-E/logged.py
@@ -0,0 +1,14 @@
+def logged(func):
+    def wrapper(*args):
+        result = func(*args)
+        return f"you called {func.__name__}{args}\nit returned {result}"
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+
+print(func(4, 4, 4))
